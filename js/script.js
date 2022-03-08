a5042c8e57a841c7ef2ed3cbda7f82634fc4c786
Steve Moritani <steve.hayato.m@gmail.com>
2022-03-08 11:05:59 -0800

    NPM Scripts Part 1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+    $(".carousel").carousel({interval : 2000});
+    $("#carouselButton").click(function(){
+        // $("#carouselPause").removeClass("bg-danger").addClass("bg-secondary");
+        // if($("#carouselPlay").hasClass("bg-secondary")){
+        //     $("#carouselPlay").removeClass("bg-secondary").addClass("bg-danger");
+        // }
+        if($("#carouselButtonIcon").hasClass("fa-pause")){
+            $("#carouselButtonIcon").removeClass("fa-pause").addClass("fa-play");
+            $(".carousel").carousel("pause");
+        } else {
+            $("#carouselButtonIcon").removeClass("fa-play").addClass("fa-pause");
+            $(".carousel").carousel("cycle");
+        }
+    });
+})
